ae738d8c678007a2395a2ac82df800015b3e05ca
Hanna Papova <seal.annuska@gmail.com>
2022-09-17 15:24:55 -0300

    feat: add db

--- db.js
+++ db.js
@@ -4,33 +4,7 @@ const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+  garage,
+  winners,
 }
 
-
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
